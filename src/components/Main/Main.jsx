01f04edf057a011a12141c72e4b26d11f0bcf18c
Thomas Aurelius <teg824@hotmail.com>
2022-06-27 23:21:47 -0500

    sizing fix

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,12 +9,12 @@ const Main = () => {
    const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardHeader title="Expense Tracker" subheader="" />
       <CardContent>
          <Typography align='center' variant='h5'>Total Balance $100</Typography>
          <Typography variant='subtitle1' style={{lineHeight: '1.5em', marginTop: '20px' }}>
             {/*  InfoCard...  */}
-            Try saying : Add income for $100 in Category Salary for Monday...
+            
          </Typography>
          <Divider />
             <Form />
